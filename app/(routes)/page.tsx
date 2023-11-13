6736be08c89790d44408a60f1f78d49536557361
Daniel <daniel.liemng@gmail.com>
2023-11-12 22:51:14 -0500

    billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import { getBillboard } from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard('4c2babca-d73f-4472-bf1e-0890dc25d5f4');
+
+  console.log(billboard);
+
+  return (
+    <div className='space-y-10 pb-10'>
+      <Billboard data={billboard} />
+    </div>
+  );
 };
 
 export default HomePage;
