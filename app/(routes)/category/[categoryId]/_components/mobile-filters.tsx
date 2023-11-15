c71776f39043b7884441036b03a451017f5bcc29
Daniel <daniel.liemng@gmail.com>
2023-11-14 22:58:46 -0500

    category page & filter

---
+++ app/(routes)/category/[categoryId]/_components/mobile-filters.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import Button from '@/components/ui/button';
+import { Color, Size } from '@/types';
+import { Fragment, useState } from 'react';
+import { Plus } from 'lucide-react';
+import { Dialog, Transition } from '@headlessui/react';
+import IconButton from '@/components/ui/icon-button';
+import { X } from 'lucide-react';
+import Filter from './filter';
+
+interface MobileFiltersProps {
+  sizes: Size[];
+  colors: Color[];
+}
+
+const MobileFilters = ({ sizes, colors }: MobileFiltersProps) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={onOpen} className='flex items-center gap-x-2 lg:hidden'>
+        Filters
+        <Plus size={20} />
+      </Button>
+      <Transition appear show={open} as={Fragment}>
+        <Dialog
+          open={open}
+          as='div'
+          className='relative z-40 lg:hidden'
+          onClose={onClose}
+        >
+          {/* Background */}
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
+          </Transition.Child>
+
+          {/* Dialog position */}
+          <div className='fixed inset-0 z-40 flex'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <Dialog.Panel className='relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl'>
+                {/* Close button */}
+                <div className='flex items-center justify-end px-4'>
+                  <IconButton icon={<X size={15} />} onClick={onClose} />
+                </div>
+
+                {/* Render filters */}
+                <div className='p-4'>
+                  <Filter valueKey='sizeId' name='Sizes' data={sizes} />
+                  <Filter valueKey='colorId' name='Colors' data={colors} />
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default MobileFilters;
